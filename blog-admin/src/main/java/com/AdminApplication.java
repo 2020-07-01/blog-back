06d2225f7fdcd68d73f60dd8c4375fb285d3e418
2020-07-01 <2422321558@qq.com>
2021-08-15 22:41:06 +0800

    nacos配置中心测试

---
+++ blog-admin/src/main/java/com/AdminApplication.java
@@ -0,0 +1,18 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName : AdminApplication
+ * @Author : yq
+ * @Date: 2021-08-15
+ * @Description :
+ */
+@SpringBootApplication
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class);
+    }
+}
