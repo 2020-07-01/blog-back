55af2f2d11aac9adb71f54a515c30cc2b46317b5
2020-07-01 <2422321558@qq.com>
2021-08-21 22:25:34 +0800

    sentinel

--- blog-facade/src/main/java/com/facade/controller/InitController.java
+++ blog-facade/src/main/java/com/facade/controller/InitController.java
@@ -1,5 +1,12 @@
 package com.facade.controller;
 
+import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
 import com.content.service.ArticleTypeService;
 import com.entity.ArticleType;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +16,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * @ClassName : InitController
@@ -25,7 +35,7 @@ public class InitController {
     private ArticleTypeService articleTypeService;
 
     @Autowired
-    private void setArticleTypeService(ArticleTypeService articleTypeService){
+    private void setArticleTypeService(ArticleTypeService articleTypeService) {
         this.articleTypeService = articleTypeService;
     }
 
@@ -33,12 +43,12 @@ public class InitController {
     private String name;
 
     @RequestMapping(value = "/name")
-    public String getName(){
+    public String getName() {
         return "真厉害";
     }
 
     @GetMapping(value = "/type")
-    public String initArticleType(){
+    public String initArticleType() {
 
         ArticleType articleType = new ArticleType();
         articleType.setArticleTypeName("java");
@@ -48,7 +58,7 @@ public class InitController {
         articleType1.setArticleTypeName("计算机网络");
         articleType1.setCreateTime(new Date());
 
-        ArticleType articleType2  = new ArticleType();
+        ArticleType articleType2 = new ArticleType();
         articleType2.setArticleTypeName("操作系统");
         articleType2.setCreateTime(new Date());
 
@@ -58,4 +68,31 @@ public class InitController {
 
         return "success";
     }
+
+    @SentinelResource("helloWorld")
+    @RequestMapping("helloWorld")
+    public String helloWorld() {
+        return "Sentinel 你大爷的!" + System.currentTimeMillis();
+    }
+
+
+    /**
+     * 用来表示资源是否被限流和降级
+     *
+     * @return
+     */
+    @SentinelResource(value = "test1", blockHandler = "blockHandlerForTest1")
+    @RequestMapping("test1")
+    public String test1() {
+        return "Sentinel 你大爷的!test1" + System.currentTimeMillis();
+    }
+
+    public String blockHandlerForTest1(BlockException blockException) {
+
+        blockException.printStackTrace();
+        return "系统繁忙，请稍后......";
+    }
+
+
+
 }
